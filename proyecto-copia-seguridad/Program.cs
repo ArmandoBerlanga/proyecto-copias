051674430ce7ced4a67fb786c69339bffa866e29
Armando Berlanga <Jose.berlangam@udem.edu>
2022-08-15 15:23:23 -0500

    primer borrador

--- proyecto-copia-seguridad/Program.cs
+++ proyecto-copia-seguridad/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
 
+// Lectrua de la configuración de la aplicación
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+// Mensaje de inicio
+Console.WriteLine("INICIANDO COPIADO DE ARCHVOS");
+
+// Lectura de archivos en paths designados
+string sourcePath = Path.Combine(Environment.CurrentDirectory, config.GetSection("Source").Get<string>());
+DirectoryInfo sourceInfo = new DirectoryInfo(sourcePath);
+
+string targetPath = Path.Combine(Environment.CurrentDirectory, config.GetSection("Target").Get<string>());
+DirectoryInfo targetInfo = new DirectoryInfo(targetPath);
+
+string[] extensions = config.GetSection("AllowedExtensiones").Get<string[]>();
+
+void CopyFiles(DirectoryInfo source, DirectoryInfo target)
+{
+    Directory.CreateDirectory(target.FullName);
+
+    var files = source.GetFiles()
+        .Where(file => extensions.Contains(file.Name.ToLower().Substring(file.Name.LastIndexOf(".") + 1)))
+        .ToList();
+
+    foreach (var file in files)
+        file.CopyTo(Path.Combine(target.FullName, file.Name), true);
+
+    foreach (var sourceSubdirectory in source.GetDirectories())
+    {
+        var targetSubdirectory = target.CreateSubdirectory(sourceSubdirectory.Name);
+        CopyFiles(sourceSubdirectory, targetSubdirectory);
+    }
+}
+
+// Ejecución de la copia de archivos y mensaje de finalización
+await Task.Run(() => CopyFiles(sourceInfo, targetInfo));
+Console.WriteLine("COPIADO DE ARCHVOS EXITOSO");
